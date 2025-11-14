453b0380d4103a86c635b7ef6b5692f08634685a
Fabiodc1981 <fabinhoted@gmail.com>
2025-11-14 16:32:12 -0300

    Add files via upload

---
+++ plm_descricao.js
@@ -0,0 +1,80 @@
+PLM_DESCRICOES = {
+    "AIQ270DX": "Observações: Devido a diferença de versões e situações de necessidade do provisionamento de segurança a BIOS apresentará algumas perguntas para que seja feito o processo correto. Grava patrimônio após gravação da Data, caso não tenha patrimônio digite novamente o número de série.",
+    "CT4KU11": "Observações: Grava patrimônio após gravação da Data, caso não tenha patrimônio digite novamente o número de série.",
+    "DN50E": "Observações: Grava patrimônio após gravação da Data, caso não tenha patrimônio digite novamente o número de série.",
+    "DN50E-RPL": "Observações: Grava patrimônio após gravação da Data, caso não tenha patrimônio digite novamente o número de série.",
+    "DH8KL01": "Observações: Grava patrimônio após gravação da Data, caso não tenha patrimônio digite novamente o número de série.",
+    "EAA75DE": "Observações: Grava patrimônio após gravação da Data, caso não tenha patrimônio digite novamente o número de série.",
+    "EIH110EA": "Observações: Grava patrimônio após gravação da Data, caso não tenha patrimônio digite novamente o número de série.",
+    "EIH310EF": "Observações: Grava patrimônio após gravação da Data, caso não tenha patrimônio digite novamente o número de série.",
+    "ES1AU11": "Observações: Grava patrimônio após gravação da Data, caso não tenha patrimônio digite novamente o número de série.",
+    "JN1-116P-HS": "Observações: Grava patrimônio após gravação da Data, caso não tenha patrimônio digite novamente o número de série.",
+    "JN1-116PC-HS": "Observações: Grava patrimônio após gravação da Data, caso não tenha patrimônio digite novamente o número de série.",
+    "MTN50": "Observações: Grava patrimônio após gravação da Data, caso não tenha patrimônio digite novamente o número de série.",
+    "M14KL01": "Observações: Grava patrimônio após gravação da Data, caso não tenha patrimônio digite novamente o número de série.",
+    "M14KR01": "Observações: Grava patrimônio após gravação da Data, caso não tenha patrimônio digite novamente o número de série.",
+    "N14DP9": "Observações: Grava patrimônio após gravação da Data, caso não tenha patrimônio digite novamente o número de série.",
+    "N14KP6-TG": "Observações: Grava patrimônio após gravação da Data, caso não tenha patrimônio digite novamente o número de série.",
+    "NB50TH": "Observações: Grava patrimônio após gravação da Data, caso não tenha patrimônio digite novamente o número de série.",
+    "NB50TL-V": "Observações: Grava patrimônio após gravação da Data, caso não tenha patrimônio digite novamente o número de série.",
+    "N240BU": "Observações: Grava patrimônio após gravação da Data, caso não tenha patrimônio digite novamente o número de série.",
+    "N240WU e N240WU-V": "Observações: Grava patrimônio após gravação da Data, caso não tenha patrimônio digite novamente o número de série.",
+    "NP11G-*": "Observações: Para os produtos (SKUS) do projeto H3-03229 que utilizam as placas NP11G-* deve ser buscado a BIOS neste documento pelo código da PLM. Esta alteração foi necessária devido à grande variação de PLMs e suas equivalências.",
+    "PB50RC-V": "Observações: Grava patrimônio após gravação da Data, caso não tenha patrimônio digite novamente o número de série.",
+    "PIB150DR": "Observações: Grava patrimônio após gravação da Data, caso não tenha patrimônio digite novamente o número de série.",
+    "PIB150DT": "Observações: Devido a diferença de versões e situações de necessidade do provisionamento de segurança a BIOS apresentará algumas perguntas para que seja feito o processo correto.",
+    "PIH110DV": "Observações:  Grava patrimônio após gravação da Data.",
+    "PIH81DI": "Observações: Grava patrimônio após gravação da Data, caso não tenha patrimônio digite novamente o número de série.",
+    "PIH81DL": "Observações: Grava patrimônio após gravação da Data, caso não tenha patrimônio digite novamente o número de série.",
+    "PIQ170DO": "<br><strong>Projeto:</strong> Fundação Canoatec H3-01953 (Windows)<br><strong>Versões de BIOS:</strong> 0320A.U. (Windows)<br><strong>Observações:</strong> Grava patrimônio após gravação da Data, caso não tenha patrimônio digite novamente o número de série.",
+    "PIQ170DS": "Observações: Grava patrimônio após gravação da Data, caso não tenha patrimônio digite novamente o número de série. Atualização de BIOS em duas etapas para algumas versões. Em caso de erro Boot Guard, enviar os logs e solicitar troca da PLM.",
+    "PIQ170DU": "Observações: Deve ser utilizado o arquivo específico buscando pelo número de série ou código da PLM.",
+    "RAA320EI": "Observações: Devido a diferença de versões e situações de necessidade do provisionamento de segurança a BIOS apresentará algumas perguntas para que seja feito o processo correto.Grava patrimônio após gravação da Data, caso não tenha patrimônio digite novamente o número de série.",
+    "RAX300ES": "<strong>Observações:</strong> O SKU 1507755 foi produzido com dois códigos de PLMs diferentes que requerem BIOS diferentes. Quando buscar os SKUs e aparecerem ambos, verificar o código da PLM para aplicar a BIOS correta.<br><strong>Para PLM:</strong> <span style='color:red'>11165342 PLM POS-RAX300ES BASIC DDR4 BOREO</span><br><strong>Utilizar BIOS:</strong> <span style='color:red'>11202932   BIOS RAX300ES_0300.3.XD10.H2.P_NS</span><br><strong>Para PLM:</strong> <span style='color:darkgreen'>11165341 PLM POS-RAX300ES PLUS V001 DDR4 BOREO</span><br><strong>Utilizar BIOS:</strong> <span style='color:darkgreen'>11205981   BIOS POS_RAX300ES_0300.4.XD10.H1.P_NS</span>",
+    "RIB360EC": "Observações: Devido a diferença de versões e situações de necessidade do provisionamento de segurança a BIOS apresentará algumas perguntas para que seja feito o processo correto. Grava patrimônio após gravação da Data, caso não tenha patrimônio digite novamente o número de série.",
+    "RIB360EE": "Observações: Devido a diferença de versões e situações de necessidade do provisionamento de segurança a BIOS apresentará algumas perguntas para que seja feito o processo correto. Grava patrimônio após gravação da Data, caso não tenha patrimônio digite novamente o número de série.",
+    "RIH470EM_R104": "<strong></strong> <span style='color:red'></span><br>\
+    <strong>Placas</strong> <span style='color:red'>RIH470EM</span><br>\
+<strong>OBSERVAÇÃO:</strong> As BIOS para estas placas foram divididas em duas tabelas sendo uma delas para a revisão 1.04 e a outra para a revisão 1.05. O script irá identificar a versão de acordo com a BIOS previamente instalada na placa então só serão exibidos no menu as BIOS compatíveis com aquela versão.<br>\
+Em caso de uma BIOS na qual não seja possível identificar a versão da PLM será exibida uma tela com algumas instruções que devem ser seguidas para que possa continuar com o processo.<br>\
+Caso um mesmo SKU possua BIOS para as duas versões de placas ele estará presente nas tabelas das duas revisões e apenas uma das BIOS será exibida durante a execução do script.<br>\
+<strong>BIOS 01XX Rev. 1.04</strong><br>\
+<strong>BIOS 03XX Rev. 1.05</strong><br>\
+<strong>Placa RIH470EM Revisão 1.04 Observações:</strong><br>\
+O SKU 1307102 POS MASTER C6300 MINIPRO S8512N1XC IOS possui duas BIOS diferentes para a PLM Ver 1.04. São elas:<br>\
+<span style='color:red'>11189796 BIOS POS-RIH470EM V002 V0106.X PD</span><br>\
+<span style='color:red'>11192486 BIOS POS-RIH470EM V005 V0103.8.X.H5.P</span><br>\
+Seguir o BIT Identificação de PLMs dos produtos POS MASTER C6300 MINIPRO na página do produto para identificar qual das duas deve ser aplicado.",
+"RIH470EM_R105": "<strong></strong> <span style='color:red'></span><br>\
+    <strong>Placas</strong> <span style='color:red'>RIH470EM</span><br>\
+<strong>OBSERVAÇÃO:</strong> As BIOS para estas placas foram divididas em duas tabelas sendo uma delas para a revisão 1.04 e a outra para a revisão 1.05. O script irá identificar a versão de acordo com a BIOS previamente instalada na placa então só serão exibidos no menu as BIOS compatíveis com aquela versão.<br>\
+Em caso de uma BIOS na qual não seja possível identificar a versão da PLM será exibida uma tela com algumas instruções que devem ser seguidas para que possa continuar com o processo.<br>\
+Caso um mesmo SKU possua BIOS para as duas versões de placas ele estará presente nas tabelas das duas revisões e apenas uma das BIOS será exibida durante a execução do script.<br>\
+<strong>BIOS 01XX Rev. 1.04</strong><br>\
+<strong>BIOS 03XX Rev. 1.05</strong><br>\.",
+    "RIQ370EB": "Observações: Grava patrimônio após gravação da Data, caso não tenha patrimônio digite novamente o número de série.",
+    "RIQ370ED": "Observações: Grava patrimônio após gravação da Data, caso não tenha patrimônio digite novamente o número de série.",
+    "RIQ470EN_R104": "<strong></strong> <span style='color:red'></span><br>\
+    <strong>Placas:</strong> <span style='color:red'>RIQ470EN</span><br>\
+    <strong>OBSERVAÇÃO:</strong><br>\
+As BIOS para estas placas foram divididas em duas tabelas sendo uma delas para a revisão 1.04 e a outra para a revisão 1.05.<br>\
+O script irá identificar a versão de acordo com a BIOS previamente instalada na placa então só serão exibidos no menu as BIOS compatíveis com aquela versão.<br>\
+Em caso de uma BIOS na qual não seja possível identificar a versão da PLM será exibida uma tela com algumas instruções que devem ser seguidas para que possa continuar com o processo.<br>\
+Caso um mesmo SKU possua BIOS para as duas versões de placas ele estará presente nas tabelas das duas revisões e apenas uma das BIOS será exibida durante a execução do script.<br>",
+     "RIQ470EN_R105": "<strong></strong> <span style='color:red'></span><br>\
+    <strong>Placas:</strong> <span style='color:red'>RIQ470EN</span><br>\
+    <strong>OBSERVAÇÃO:</strong><br>\
+As BIOS para estas placas foram divididas em duas tabelas sendo uma delas para a revisão 1.04 e a outra para a revisão 1.05.<br>\
+O script irá identificar a versão de acordo com a BIOS previamente instalada na placa então só serão exibidos no menu as BIOS compatíveis com aquela versão.<br>\
+Em caso de uma BIOS na qual não seja possível identificar a versão da PLM será exibida uma tela com algumas instruções que devem ser seguidas para que possa continuar com o processo.<br>\
+Caso um mesmo SKU possua BIOS para as duas versões de placas ele estará presente nas tabelas das duas revisões e apenas uma das BIOS será exibida durante a execução do script.<br>",
+    "RIQ670FB": "O 1508235 MASTER C8400 H3-04378 SEST SENAT/DF LT1 H3-04378 possui duas BIOS diferentes para a PLM. São elas:<br>\
+Seguir o BIT Identificação de PLMs dos produtos POS MASTER C8400 MINIPRO, na página do produto para identificar qual das duas deve ser aplicado.<br>",
+    "H110T": "Observações: Grava patrimônio após gravação da Data, caso não tenha patrimônio digite novamente o número de série.",
+    "S14BW": "<strong>Projeto:</strong> IBGE<br>\
+<strong>Versões de BIOS:</strong> 1.09.L (Linux) e 1.09.X (Windows)<br>\
+<strong>Projeto:</strong><br>\
+<strong>Observações:</strong> Quando a BIOS estiver em uma versão inferior a 1.09 serão necessárias várias atualizações para que seja implementada a BIOS final.<br>",
+    "W942SW": "<strong>Versões de BIOS:</strong> 1.03.06RPOSU_W81_PST<br>\
+<strong>Observações:</strong> Quando a BIOS estiver em uma versão inferior a 1.09 serão necessárias várias atualizações para que seja implementada a BIOS final.<br>",
+};
